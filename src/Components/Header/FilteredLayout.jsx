395ffa60f75f33b16ba6f9ce6c03bdcb5eb66d03
Fariskt <fariskt00@gmail.com>
2024-10-03 21:11:16 +0530

    style changed

--- src/Components/Header/FilteredLayout.jsx
+++ src/Components/Header/FilteredLayout.jsx
@@ -1,22 +1,21 @@
 import React, { useContext, useState, useEffect } from "react";
-import { AppContext} from "../../context/AppContext";
+import { AppContext } from "../../context/AppContext";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
 import { IoCartOutline } from "react-icons/io5";
 
 const FilteredLayout = ({ setShow }) => {
-  const {filteredLayouts , dispatch } = useContext(AppContext)
+  const { filteredLayouts, dispatch } = useContext(AppContext);
   const { addToCart } = useContext(CartContext);
   const [selectedElements, setSelectedElements] = useState([]);
   const [active, setIsActive] = useState(null);
-  
+
   const navigate = useNavigate();
   const handleBaseClick = (index) => {
     let path = `layout`;
     navigate(path);
-    dispatch({type: "SET_CLICKED_INDEX" , payload: index})
+    dispatch({ type: "SET_CLICKED_INDEX", payload: index });
   };
-
 
   let data = [
     "#war",
@@ -33,37 +32,43 @@ const FilteredLayout = ({ setShow }) => {
     setSelectedElements(shuffledData.slice(0, 4));
   }, []);
   return (
-    <div className="base-container" onClick={() => setShow(false)}>
-      {filteredLayouts.map((layout, index) => (
-        <div className="base-img" key={index}>
-          <img
-            onClick={() => handleBaseClick(index)}
-            src={layout.img}
-            alt={`Image ${layout.id}`}
-          /> 
-          <p className="name">{layout.author}</p>
-          <h5
-            className={`${layout ? "add-cart" : "clicked"}`}
-            onClick={() => addToCart(layout)}
-            title="Add to cart"
-          >
-            <span
-              onClick={()=> setIsActive(layout)}
-              className={`add-cart ${active == layout && "clicked"}`}
-            >
-              <IoCartOutline />
-            </span>
-          </h5>
-          <p>
-            {selectedElements.map((element, i) => (
-              <span key={i} style={{ backgroundColor: element }}>
-                {element}
-              </span>
-            ))}
-          </p>
+    <>
+      {filteredLayouts.length > 0 ? (
+        <div className="base-container" onClick={() => setShow(false)}>
+          {filteredLayouts.map((layout, index) => (
+            <div className="base-img" key={index}>
+              <img
+                onClick={() => handleBaseClick(index)}
+                src={layout.img}
+                alt={`Image ${layout.id}`}
+              />
+              <p className="name">{layout.author}</p>
+              <h5
+                className={`${layout ? "add-cart" : "clicked"}`}
+                onClick={() => addToCart(layout)}
+                title="Add to cart"
+              >
+                <span
+                  onClick={() => setIsActive(layout)}
+                  className={`add-cart ${active == layout && "clicked"}`}
+                >
+                  <IoCartOutline />
+                </span>
+              </h5>
+              <p>
+                {selectedElements.map((element, i) => (
+                  <span key={i} style={{ backgroundColor: element }}>
+                    {element}
+                  </span>
+                ))}
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <h2 className="no-base">No Layout found</h2>
+      )}
+    </>
   );
 };
 
